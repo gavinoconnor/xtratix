34ade5a090321b84da2463ea607e6aa67442d6a6
virginia koar <virginiakoar@TimKoar.local>
2019-05-24 13:18:07 -0400

    first commit self referentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+  has_many :tickets
+  has_many :venues, through: :tickets
 end
