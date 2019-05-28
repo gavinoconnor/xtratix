d5a5ecbe62199dd18cd4a3a6215b3142bdf2d2fd
Tim Koar <koartimothy@gmail.com>
2019-05-28 10:34:07 -0400

    added application controller methods and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :venues
   resources :users
 
-  # patch '/friend_list', to: 'friend_list#update'
-
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 end
