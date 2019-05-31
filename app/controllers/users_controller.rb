45a18988ea931c61d05bf2570d6d38a1571073a8
gavinoconnor <gedmund@gmail.com>
2019-05-31 12:38:24 -0400

    Tweakin'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
 
    def show
      @user = User.find(params[:id])
+     # flash.now.notice = "Ticket request has been sent!"
    end
 
    def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :venues
   resources :users
   resources :friendships
+  resources :sessions
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
